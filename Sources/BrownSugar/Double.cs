3247907bbd2a16d7edf407e9f2fdaf42f4a550f0
m5knt <m5knt@inetsbb.com>
2015-10-08 02:06:37 +0900

    to short name

--- Sources/BrownSugar/Double.cs
+++ Sources/BrownSugar/Double.cs
@@ -17,23 +17,19 @@ namespace BrownSugar {
     public static class DoubleSugar {
 
         public static bool IsInfinity(this double f) {
-            return Double.IsInfinity(f);
+			return double.IsInfinity(f);
         }
 
-        public static bool IsNaN(this Double f) {
-            return Double.IsNaN(f);
+        public static bool IsNaN(this double f) {
+			return double.IsNaN(f);
         }
 
-        public static bool IsNegativeInfinity(this Double f) {
-            return Double.IsNegativeInfinity(f);
+		public static bool IsNegativeInfinity(this double f) {
+			return double.IsNegativeInfinity(f);
         }
 
-        public static bool IsPositiveInfinity(this Double f) {
-            return Double.IsPositiveInfinity(f);
-        }
-
-        public static long To64Bits(this double val) {
-            return BitConverter.DoubleToInt64Bits(val);
+		public static bool IsPositiveInfinity(this double f) {
+			return double.IsPositiveInfinity(f);
         }
     }
 }
