8d7191a9cbc33853caef353b00260f212245b2ad
m5knt <m5knt@inetsbb.com>
2015-10-11 23:30:16 +0900

    add base namespace source

---
+++ Sources/BrownSugar/BrownSugar.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * @file
+ * @Auther Yukio KANEDA
+ */
+
+using System;
+using System.Net;
+
+/*
+ *
+ */
+
+/// <summary>
+/// バイト・ビットに関係するシンタックスシュガーを纏めています
+/// </summary>
+namespace ThunderEgg.BrownSugar {
+
+    /// <summary>
+    /// 拡張メソッドによるシュガー
+    /// </summary>
+    public static partial class Sugar {
+    }
+}
+
