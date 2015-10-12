7775c8f36a27ab6c54b1506f05560739a48d243a
m5knt <m5knt@inetsbb.com>
2015-10-12 23:25:13 +0900

    add perf bench

---
+++ Test/Bench.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using ThunderEgg.BrownSugar;
+
+namespace Test {
+
+    public static class Bench {
+        public static long Run(int limit, Action<long> action) {
+            var now = DateTime.Now;
+            var count = 0L;
+            while ((DateTime.Now - now).TotalSeconds < limit) {
+                action(count);
+                ++count;
+            }
+            Console.WriteLine(action.Method.DeclaringType.ToString() + ":" + count.ToString());
+            return count;
+        }
+    }
+}
