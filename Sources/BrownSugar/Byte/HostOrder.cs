7c003d7eb1486e1fc0e28b9797c6007732bbbfe7
m5knt <m5knt@inetsbb.com>
2015-10-22 00:13:47 +0900

    スタイル変更

--- Sources/BrownSugar/Byte/HostOrder.cs
+++ Sources/BrownSugar/Byte/HostOrder.cs
@@ -32,20 +32,17 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe ushort ToUInt16(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(ushort*)b;
+            fixed (byte* b = &buffer[index]) return *(ushort*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe short ToInt16(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(short*)b;
+            fixed (byte* b = &buffer[index]) return *(short*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe char ToChar(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(char*)b;
+            fixed (byte* b = &buffer[index]) return *(char*)b;
         }
 
         //
@@ -69,20 +66,17 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe uint ToUInt32(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(uint*)b;
+            fixed (byte* b = &buffer[index]) return *(uint*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe int ToInt32(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(int*)b;
+            fixed (byte* b = &buffer[index]) return *(int*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe float ToSingle(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(float*)b;
+            fixed (byte* b = &buffer[index]) return *(float*)b;
         }
 
         //
@@ -106,20 +100,17 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe ulong ToUInt64(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(ulong*)b;
+            fixed (byte* b = &buffer[index]) return *(ulong*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe long ToInt64(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(long*)b;
+            fixed (byte* b = &buffer[index]) return *(long*)b;
         }
 
         /// <summary>アライメント済みバッファ位置の値をホストオーダー順で取得</summary>
         public static unsafe double ToDouble(byte[] buffer, int index) {
-            fixed (byte* b = &buffer[index])
-            return *(double*)b;
+            fixed (byte* b = &buffer[index]) return *(double*)b;
         }
 
         //
@@ -143,20 +134,17 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, ushort value) {
-            fixed (byte* b = &buffer[index])
-            *(ushort*)b = value;
+            fixed (byte* b = &buffer[index]) *(ushort*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, short value) {
-            fixed (byte* b = &buffer[index])
-            *(short*)b = value;
+            fixed (byte* b = &buffer[index]) *(short*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, char value) {
-            fixed (byte* b = &buffer[index])
-            *(char*)b = value;
+            fixed (byte* b = &buffer[index]) *(char*)b = value;
         }
 
         //
@@ -180,20 +168,17 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, uint value) {
-            fixed (byte* b = &buffer[index])
-            *(uint*)b = value;
+            fixed (byte* b = &buffer[index]) *(uint*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, int value) {
-            fixed (byte* b = &buffer[index])
-            *(int*)b = value;
+            fixed (byte* b = &buffer[index]) *(int*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, float value) {
-            fixed (byte* b = &buffer[index])
-            *(float*)b = value;
+            fixed (byte* b = &buffer[index]) *(float*)b = value;
         }
 
         //
@@ -217,23 +202,23 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, ulong value) {
-            fixed (byte* b = &buffer[index])
-            *(ulong*)b = value;
+            fixed (byte* b = &buffer[index]) *(ulong*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, long value) {
-            fixed (byte* b = &buffer[index])
-            *(long*)b = value;
+            fixed (byte* b = &buffer[index]) *(long*)b = value;
         }
 
         /// <summary>アライメント済みバッファ位置に値をホストオーダー順で書く</summary>
         public static unsafe void Assign(byte[] buffer, int index, double value) {
-            fixed (byte* b = &buffer[index])
-            *(double*)b = value;
+            fixed (byte* b = &buffer[index]) *(double*)b = value;
         }
-
     }
+
+    //
+    //
+    //
 
     public static class HostOrder {
 
@@ -245,9 +230,9 @@ namespace ThunderEgg.BrownSugar {
             }
             unsafe
             {
-                fixed (byte* fix = buffer)
+                fixed (byte* fix = &buffer[index])
                 {
-                    Marshal.StructureToPtr(obj, new IntPtr(fix + index), false);
+                    Marshal.StructureToPtr(obj, new IntPtr(fix), false);
                 }
             }
         }
@@ -286,9 +271,9 @@ namespace ThunderEgg.BrownSugar {
             }
             unsafe
             {
-                fixed (byte* fix = buffer)
+                fixed (byte* fix = &buffer[index])
                 {
-                    return (T)Marshal.PtrToStructure(new IntPtr(fix + index), type);
+                    return (T)Marshal.PtrToStructure(new IntPtr(fix), type);
                 }
             }
         }
