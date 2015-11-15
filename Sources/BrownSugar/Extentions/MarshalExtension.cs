07aefc07732dd98e43ed0342d3af7d20118aba5a
m5knt <m5knt@inetsbb.com>
2015-11-15 17:32:19 +0900

    マーシャル関係のエクステンション追加

---
+++ Sources/BrownSugar/Extentions/MarshalExtension.cs
@@ -0,0 +1,46 @@
+﻿/**
+* @file
+* @brief マーシャル関係のエクステンション
+*/
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace ThunderEgg.BrownSugar.Extentions {
+
+    /// <summary>マーシャル関係のエクステンション</summary>
+    public static class MarshalExtension {
+
+        /// <summary>マーシャル時のサイズを返す</summary>
+        public static int MarshalSize(this Type type) {
+            return Marshal.SizeOf(type);
+        }
+
+        /// <summary>マーシャル時のサイズを返す</summary>
+        public static int MarshalSize<T>(this T self) {
+            return Marshal.SizeOf(self);
+        }
+
+        /// <summary>マーシャルアトリビュートのサイズを返す</summary>
+        public static int MarshalCount(this Type type, string name) {
+            return ByteOrder.MarshalCount(type, name);
+        }
+
+        /// <summary>マーシャルアトリビュートのサイズを返す</summary>
+        public static int MarshalCount<T>(this T self, string name) {
+            return ByteOrder.MarshalCount(self, name);
+        }
+
+        /// <summary>マーシャルアトリビュートのオフセットを返す</summary>
+        public static int MarshalOffset(this Type type, string name) {
+            return Marshal.OffsetOf(type, name).ToInt32();
+        }
+
+        /// <summary>マーシャルアトリビュートのオフセットを返す</summary>
+        public static int MarshalOffset<T>(this T self, string name) {
+            return Marshal.OffsetOf(typeof(T), name).ToInt32();
+        }
+
+    }
+}
+
