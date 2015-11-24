491a63185665e8c1504bf274e15e507f5423de3c
m5knt <m5knt@inetsbb.com>
2015-11-25 01:57:16 +0900

    オブジェクトプール追加

---
+++ Test/ObjectPool.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using ThunderEgg.BrownSugar;
+using ThunderEgg.BrownSugar.Extentions;
+
+namespace Test {
+
+    [TestClass]
+    public class ObjectPool_ {
+
+        [TestMethod]
+        public void ObjectPool() {
+            var pool = new ObjectPool<byte[]>(
+                () => {
+                    return new byte[1500];
+                }, 100);
+            using (var n = pool.Allocate()) {
+                var t = n.Value;
+            }
+        }
+    }
+}
+
