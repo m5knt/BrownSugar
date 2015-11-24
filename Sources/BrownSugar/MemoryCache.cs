8157c568d9e2d2594f5d1efb2d15bd519532017c
m5knt <m5knt@inetsbb.com>
2015-11-25 02:00:33 +0900

    オブジェクトプール追加

---
+++ Sources/BrownSugar/MemoryCache.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * @file
+ * @brief メモリーキャッシュ関係
+ */
+
+using System;
+using System.Collections.Generic;
+
+/*
+ *
+ */
+
+namespace ThunderEgg.BrownSugar {
+
+    /// <summary>メモリーキャッシュ関係</summary>
+    public class MemoryQuiche<Key, Value> {
+
+        Dictionary<Key, WeakReference> Weaks;
+
+        public MemoryQuiche() {
+            Weaks = new Dictionary<Key, WeakReference>();
+        }
+
+        public bool TryGetValue(Key key, ref Value value) {
+            WeakReference r;
+            if (Weaks.TryGetValue(key, out r)) {
+                var n = r.Target;
+                if (n != null && r.IsAlive) {
+                    value = (Value)n;
+                    return true;
+                }
+                Weaks.Remove(key);
+                return false;
+            }
+            return false;
+        }
+
+#if false
+        public bool LifeExtension(K key) {
+        }
+
+        public void Reload(K key) {
+
+        }
+#endif
+    }
+}
+
+//
+//
+//
+
--- Sources/BrownSugar/ObjectPool.cs
+++ Sources/BrownSugar/ObjectPool.cs
@@ -1,4 +1,6 @@
-﻿/**
+﻿
+
+/**
  * @file
  * @brief メモリーキャッシュ関係
  */
@@ -12,40 +14,108 @@ using System.Collections.Generic;
 
 namespace ThunderEgg.BrownSugar {
 
-    /// <summary>メモリーキャッシュ関係</summary>
-    public class MemoryQuiche<Key, Value> {
+	public class ObjectHolder<TValue> : IDisposable //
+		where TValue : class //
+	{
+		public TValue Value { get; protected set; }
 
-        Dictionary<Key, WeakReference> Weaks;
+		TValue Shadow;
+		ObjectPool<TValue> Pool;
 
-        public MemoryQuiche() {
-            Weaks = new Dictionary<Key, WeakReference>();
-        }
+		public ObjectHolder(ObjectPool<TValue> pool, TValue val) {
+			Pool = pool;
+			Shadow = val;
+		}
 
-        public bool TryGetValue(Key key, ref Value value) {
-            WeakReference r;
-            if (Weaks.TryGetValue(key, out r)) {
-                var n = r.Target;
-                if (n != null && r.IsAlive) {
-                    value = (Value)n;
-                    return true;
-                }
-                Weaks.Remove(key);
-                return false;
-            }
-            return false;
-        }
+		public void Dispose() {
+			if (Value == null) {
+				return;
+			}
+			Value = null;
+			if (Pool.IsDispose) {
+				RealDispose();
+			}
+			else {
+				Pool.Free(this);
+			}
+		}
 
-#if false
-        public bool LifeExtension(K key) {
-        }
+		public void RealDispose() {
+			using (var dis = Shadow as IDisposable) {
+			}
+			Shadow = null;
+			Pool = null;
+		}
 
-        public void Reload(K key) {
+		public void ReUse(ObjectPool<TValue> n) {
+			Value = Shadow;
+		}
+	}
 
-        }
-#endif
-    }
+	//
+	//
+	//
+
+	public class ObjectPool<TValue>
+		where TValue : class //
+	{
+		Object Object = new object();
+		Queue<ObjectHolder<TValue>> Frees;
+		Queue<ObjectHolder<TValue>> Allocs;
+		int Capacity;
+		Func<TValue> Creater;
+
+		public ObjectPool(Func<TValue> creater, int capacity) {
+			Frees = new Queue<ObjectHolder<TValue>>();
+			Allocs = new Queue<ObjectHolder<TValue>>();
+			Capacity = capacity;
+			Creater = creater;
+		}
+
+		public ObjectHolder<TValue> Allocate() {
+			lock (Object) {
+				ObjectHolder<TValue> holder;
+				if (Frees.Count > 0) {
+					holder = Frees.Dequeue();
+				}
+				else {
+					holder = new ObjectHolder<TValue>(this, Creater());
+				}
+				Allocs.Enqueue(holder);
+				holder.ReUse(this);
+				return holder;
+			}
+		}
+
+		public void Free(ObjectHolder<TValue> holder) {
+			lock (Object) {
+				Frees.Enqueue(holder);
+			}
+		}
+
+		public void Dispose() {
+			if (Object == null) {
+				return;
+			}
+			lock (Object) {
+				foreach (var n in Frees) {
+					n.RealDispose();
+				}
+				Allocs = null;
+				Frees = null;
+				Creater = null;
+			}
+			Object = null;
+			GC.SuppressFinalize(this);
+		}
+
+		public bool IsDispose {
+			get {
+				return Object == null;
+			}
+		}
+	}
 }
-
 //
 //
 //
