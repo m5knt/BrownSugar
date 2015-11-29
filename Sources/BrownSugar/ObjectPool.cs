e9d9993805fec0f6a1f0935a01fc76e88fe1fa4b
m5knt <m5knt@inetsbb.com>
2015-11-30 01:07:15 +0900

    コードクリーンナップ

--- Sources/BrownSugar/ObjectPool.cs
+++ Sources/BrownSugar/ObjectPool.cs
@@ -14,108 +14,53 @@ namespace ThunderEgg.BrownSugar {
 
     /// <summary>オブジェクト管理情報</summary>
     public class ObjectPoolStat {
-        /// <summary>オブジェクト</summary>
+        /// <summary>オブジェクトホルダー</summary>
         public IObjectHolder Holder;
-        /// <summary>オブジェクト弱参照</summary>
+        /// <summary>オブジェクトホルダーへの弱参照</summary>
         public WeakReference Weak;
     }
-
-    public delegate IObjectHolder ObjectHolderCreater();
 
     /// <summary>オブジェクトプールのオブジェクトを保持するインターフェース</summary>
     public interface IObjectHolder : IDisposable {
         /// <summary>ディスポーズ済みかを返す</summary>
-        bool IsFakeDisposed { get; }
+        bool IsDisposed { get; }
         /// <summary>本当のディスポーズ</summary>
         void RealDispose();
+        /// <summary>再利用</summary>
+        void Reuse();
 
         /// <summary>オブジェクト管理元</summary>
         IObjectPool Pool { get; set; }
         /// <summary>オブジェクト管理情報</summary>
-        ObjectPoolStat Stat { get; set; }
-        /// <summary>再利用</summary>
-        void Reuse();
-    }
-
-    /// <summary>オブジェクトプールのオブジェクトを保持するインターフェース</summary>
-    public interface IObjectHolder<TValue> : IObjectHolder {
-        /// <summary>オブジェクトの値</summary>
-        TValue Value { get; }
+        ObjectPoolStat PoolStat { get; set; }
     }
 
     /// <summary>オブジェクトプールのインターフェース</summary>
     public interface IObjectPool : IDisposable {
+
+        /// <summary>ディスポーズ済みかを返す</summary>
+        bool IsDisposed { get; }
+
+        /// <summary>プールしているオブジェクト数</summary>
+        int Count { get; }
+
         /// <summary>オブジェクトを確保します</summary>
         IObjectHolder Allocate();
+
         /// <summary>プールへ戻す</summary>
         /// <returns>成否を返す</returns>
         bool Free(IObjectHolder holder);
-        /// <summary>プールしているオブジェクト数</summary>
-        int Count { get; }
-    }
 
-    //
-    //
-    //    
-    
-    /// <summary>オブジェクトプール用のオブジェクトを保持</summary>
-    public class ObjectHolder<TValue> : IObjectHolder<TValue> //
-        where TValue : class //
-    {
-        /// <summary>保持するオブジェクト</summary>
-        public TValue Value { get; protected set; }
-
-        /// <summary>ディスポーズされているか</summary>
-        public bool IsFakeDisposed { get; protected set; }
-
-        /// <summary>管理元プール</summary>
-        public IObjectPool Pool { get; set; }
-
-        /// <summary>管理キー</summary>
-        public ObjectPoolStat Stat { get; set; }
-
-        //
-        //
-        //
-
-        /// <summary>コンストラクタ</summary>
-        public ObjectHolder(TValue val) {
-            IsFakeDisposed = true;
-            Value = val;
-        }
-
-        /// <summary>再利用</summary>
-        public void Reuse() {
-            IsFakeDisposed = false;
-        }
-
-        /// <summary>ディスポーズ</summary>
-		public void Dispose() {
-            if (IsFakeDisposed) {
-                return;
-            }
-            IsFakeDisposed = true;
-            // プールがあるなら返す
-            if (Pool.Free(this)) {
-                return;
-            }
-            // 実際にディスポーズ
-            RealDispose();
-            GC.SuppressFinalize(this);
-        }
-
-        /// <summary>本当のディスポーズ</summary>
-        public void RealDispose() {
-            using (var dis = Value as IDisposable) { }
-            Stat = null;
-            Value = null;
-            Pool = null;
-        }
+        /// <summary>GC回収を知らせる</summary>
+        void HolderCollect(IObjectHolder holder);
     }
 
     //
     //
     //
+
+    /// <summary>オブジェクト生成器</summary>
+    public delegate IObjectHolder ObjectHolderCreater();
 
     /// <summary>オブジェクトプール</summary>
     public class ObjectPool : IObjectPool {
@@ -136,6 +81,16 @@ namespace ThunderEgg.BrownSugar {
             }
         }
 
+        /// <summary>貸出しているオブジェクト数</summary>
+        public int LeasedCount {
+            get {
+                lock (this) {
+                    CheckDisposed();
+                    return Leased.Count;
+                }
+            }
+        }
+
         /// <summary>ディスポーズ済みかを返す</summary>
         volatile bool IsDisposed_;
 
@@ -147,6 +102,7 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>オブジェクト生成器</summary>
         ObjectHolderCreater Creater;
+
         /// <summary>保持量</summary>
         int Limit;
 
@@ -180,21 +136,21 @@ namespace ThunderEgg.BrownSugar {
             }
             // 取得できたら貸出
             if (node != null) {
-                return DoLeased(node);
+                return Lender(node);
             }
             // 新規に作成して貸出
-            var holder = Creater();
             var stat = new ObjectPoolStat();
+            var holder = Creater();
             holder.Pool = this;
-            holder.Stat = stat;
+            holder.PoolStat = stat;
             stat.Holder = holder;
             stat.Weak = new WeakReference(stat.Holder);
             node = new LinkedListNode<ObjectPoolStat>(stat);
-            return DoLeased(node);
+            return Lender(node);
         }
 
         /// <summary>貸出状態にする</summary>
-        IObjectHolder DoLeased(LinkedListNode<ObjectPoolStat> node) {
+        IObjectHolder Lender(LinkedListNode<ObjectPoolStat> node) {
             // 未返却対応で弱監視にする
             var holder = (IObjectHolder)node.Value.Holder;
             node.Value.Holder = null;
@@ -210,15 +166,38 @@ namespace ThunderEgg.BrownSugar {
 
         /// <summary>プールへ戻す</summary>
         public bool Free(IObjectHolder holder) {
-            holder.Stat.Holder = holder;
             lock (this) {
-                if (IsDisposed) return false;
+                if (IsDisposed) goto HolderDisposer;
                 // 貸出表から消す
-                Leased.Remove(holder.Stat);
-                // 十分な数があればプールに入れない
-                if (Pooled.Count >= Limit) return false;
-                Pooled.AddLast(holder.Stat);
+                Leased.Remove(holder.PoolStat);
+                CheckCollectedHolder();
+                // 制限内ならプールへ戻す
+                if (Pooled.Count >= Limit) goto HolderDisposer;
+                holder.PoolStat.Holder = holder;
+                Pooled.AddLast(holder.PoolStat);
                 return true;
+            }
+        HolderDisposer:
+            HolderDisposer(holder);
+            return false;
+        }
+
+        /// <summary>ホルダーをディスポーズをする</summary>
+        void HolderDisposer(IObjectHolder holder) {
+            holder.PoolStat = null;
+            holder.Pool = null;
+            holder.RealDispose();
+        }
+
+        /// <summary>GC済みを管理から外す</summary>
+        void CheckCollectedHolder() {
+            lock (this) {
+                var n = Leased.GetEnumerator();
+                while (n.MoveNext()) {
+                    if (!n.Current.Weak.IsAlive) {
+                        Leased.Remove(n.Current);
+                    }
+                }
             }
         }
 
@@ -235,15 +214,26 @@ namespace ThunderEgg.BrownSugar {
                 Leased = null;
             }
             foreach (var item in pooled) {
-                item.Holder.RealDispose();
+                HolderDisposer(item.Holder);
+                item.Holder = null;
+                item.Weak = null;
             }
             pooled.Clear();
             leased.Clear();
             Creater = null;
             GC.SuppressFinalize(this);
         }
-    }
 
+        /// <summary>ホルダーのGC回収を知らてもらう</summary>
+        public void HolderCollect(IObjectHolder holder) {
+            lock (this) {
+                if (IsDisposed) return;
+                holder.PoolStat.Weak = null;
+                Leased.Remove(holder.PoolStat);
+            }
+        }
+
+    }
 
     /// <summary>明示されたオブジェクトを管理するオブジェクトプール</summary>
     public class ObjectPool<THolder> : ObjectPool //
@@ -258,6 +248,75 @@ namespace ThunderEgg.BrownSugar {
         /// <summary>プールからオブジェクトを確保する</summary>
         public new THolder Allocate() {
             return (THolder)base.Allocate();
+        }
+    }
+
+    //
+    //
+    //
+
+    /// <summary>オブジェクトプール用のオブジェクトを保持</summary>
+    public abstract class ObjectHolderBase : IObjectHolder //
+    {
+        /// <summary>ディスポーズされているか</summary>
+        public bool IsDisposed { get; protected set; }
+
+        /// <summary>管理元プール</summary>
+        public IObjectPool Pool { get; set; }
+
+        /// <summary>管理キー</summary>
+        public ObjectPoolStat PoolStat { get; set; }
+
+        /// <summary>コンストラクタ</summary>
+        protected ObjectHolderBase() {
+            IsDisposed = true;
+        }
+
+        /// <summary>ファイナライザ</summary>
+        ~ObjectHolderBase() {
+            if (Pool != null) {
+                Pool.HolderCollect(this);
+            }
+        }
+
+        /// <summary>再利用</summary>
+        public void Reuse() {
+            IsDisposed = false;
+        }
+
+        /// <summary>ディスポーズ</summary>
+		public void Dispose() {
+            if (IsDisposed) {
+                return;
+            }
+            IsDisposed = true;
+            // プールがあるなら返す
+            if (Pool.Free(this)) {
+                return;
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>本当のディスポーズ</summary>
+        public abstract void RealDispose();
+    }
+
+    /// <summary>オブジェクトプール用のオブジェクトを保持</summary>
+    public class ObjectHolder<TValue> : ObjectHolderBase //
+        where TValue : class //
+    {
+        /// <summary>保持するオブジェクト</summary>
+        public TValue Value { get; protected set; }
+
+        /// <summary>コンストラクタ</summary>
+        public ObjectHolder(TValue val) {
+            Value = val;
+        }
+
+        /// <summary>本当のディスポーズ</summary>
+        public override void RealDispose() {
+            using (var dis = Value as IDisposable) { }
+            Value = null;
         }
     }
 }
--- Test/ObjectPool.cs
+++ Test/ObjectPool.cs
@@ -13,13 +13,15 @@ namespace Test {
     public class ObjectPool_ {
 
         static int collect_count = 0;
+
         ObjectPool<Holder> pool = new ObjectPool<Holder>(
-            () => {
-                return new Holder(new byte[1500]);
-            }, 2);
+            () => new Holder(), 2);
 
         public class Holder : ObjectHolder<byte[]> {
-            public Holder(byte[] value) : base(value) { }
+            public Holder() : base(new byte[1500])
+            {
+            }
+
             ~Holder() {
                 Interlocked.Increment(ref collect_count);
             }
@@ -28,37 +30,65 @@ namespace Test {
         void ObjectPoolSub() {
             Holder one, two, three, four;
 
-            one = pool.Allocate(); // pool 0
+            // pool 0, lease 1
+            one = pool.Allocate();
+            Assert.AreEqual(1, pool.LeasedCount);
             Assert.AreEqual(0, pool.Count);
-            one.Dispose(); // pool 1
+            // pool 1, lease 0
+            one.Dispose();
+            Assert.AreEqual(0, pool.LeasedCount);
             Assert.AreEqual(1, pool.Count);
-            //
-            two = pool.Allocate(); // pool 0 再利用
+            // pool 0, lease 1 再利用
+            two = pool.Allocate();
             Assert.AreSame(one, two);
+            Assert.AreEqual(1, pool.LeasedCount);
             Assert.AreEqual(0, pool.Count);
-            //
-            three = pool.Allocate();　// pool 0　新規
+            // pool 0, lease 2 新規
+            three = pool.Allocate();
+            Assert.AreEqual(2, pool.LeasedCount);
+            Assert.AreEqual(0, pool.Count);
             Assert.AreNotSame(three, two);
-            four = pool.Allocate();　// pool 0　新規
-            //
-            two.Dispose(); // pool 1
+            // pool 0, lease 3 新規
+            four = pool.Allocate();
+            Assert.AreEqual(3, pool.LeasedCount);
+            Assert.AreEqual(0, pool.Count);
+
+            // pool 1, lease 2
+            two.Dispose();
+            Assert.AreEqual(2, pool.LeasedCount);
             Assert.AreEqual(1, pool.Count);
-            three.Dispose(); // pool 2
+            // pool 2, lease 2
+            three.Dispose();
+            Assert.AreEqual(1, pool.LeasedCount);
             Assert.AreEqual(2, pool.Count);
-            four.Dispose(); // pool 2
+            // pool 2, lease 1
+            four.Dispose();
+            Assert.AreEqual(0, pool.LeasedCount);
             Assert.AreEqual(2, pool.Count);
-            // for gc collect
+            // pool 1, lease 1, gc 0, for gc collect 
             var t = pool.Allocate();
+            Assert.AreEqual(1, pool.Count);
+            Assert.AreEqual(1, pool.LeasedCount);
         }
 
         [TestMethod]
         public void ObjectPool() {
             ObjectPoolSub();
             Assert.AreEqual(0, collect_count);
+            // pool 1, lease 0, gc 1
             GC.Collect();
             GC.WaitForPendingFinalizers();
             GC.Collect();
             Assert.AreEqual(1, collect_count);
+            Assert.AreEqual(0, pool.LeasedCount);
+            Assert.AreEqual(1, pool.Count);
+            // gc 2
+            pool.Dispose();
+            pool = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Assert.AreEqual(2, collect_count);
         }
     }
 
