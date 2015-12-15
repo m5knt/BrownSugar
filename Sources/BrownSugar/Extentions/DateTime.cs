be62b3fc2c5116312d7d16d988230f5192a33128
m5knt <m5knt@inetsbb.com>
2015-12-16 00:52:32 +0900

    unixtime変換追加

---
+++ Sources/BrownSugar/Extentions/DateTime.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * @file
+ * @brief DateTimeの拡張メソッド関係
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ThunderEgg.BrownSugar.Extentions {
+
+    /// <summary>DateTimeの拡張メソッド関係</summary>
+    public static class DateTimeExtension {
+		static DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+		public static long ToUnixTime(this DateTime self) {
+			return (long)(self - UnixEpoch).TotalSeconds;
+		}
+	}
+}
+
