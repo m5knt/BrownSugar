d6ad8248b8dbce9c6d0588f390af5493262f0abb
m5knt <m5knt@inetsbb.com>
2015-12-16 13:02:48 +0900

    bom無しutf8エンコード取得

---
+++ Sources/BrownSugar/Extentions/EncodingExtension.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * @file
+ */
+
+using System;
+using System.Text;
+
+namespace ThunderEgg.BrownSugar.Extentions {
+
+    /// <summary>Encodingの拡張メソッド関係</summary>
+	public static class EncodingExtension {
+
+		static Encoding UTF8N_ = new UTF8Encoding(false);
+
+        public static Encoding UTF8N(this string self) {
+			return UTF8N_;
+        }
+    }
+}
+
