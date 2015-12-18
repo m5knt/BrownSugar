bc976e6d619f121faf9baa2c875ccca67bb82430
m5knt <m5knt@inetsbb.com>
2015-12-18 14:11:07 +0900

    unixタイム取得機能追加

---
+++ Sources/BrownSugar/DateTimeTool.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * @file
+ * @brief 日付関連
+ */
+
+using System;
+
+/*
+ *
+ */
+
+namespace ThunderEgg.BrownSugar {
+
+    /// <summary>日付関連</summary>
+    public class DateTimeTool {
+
+        /// <summary>Unixエポックタイム</summary>
+        static DateTime UnixEpoch = new DateTime(1970, 1, 1);
+
+        /// <summary>Unixタイムを返す</summary>
+        public static long NowUnixTime() {
+            return (long)(DateTime.UtcNow - UnixEpoch).TotalSeconds;
+        }
+    }
+}
+
+
