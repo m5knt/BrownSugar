e0a64e0e631bea48e46de82e3ddd66d132513c5b
m5knt <m5knt@inetsbb.com>
2015-12-18 14:48:39 +0900

    シングルトン追加

---
+++ Sources/BrownSugar/Singleton.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * @file
+ * @brief シングルトン
+ */
+
+using System;
+
+/*
+ *
+ */
+
+namespace ThunderEgg.BrownSugar {
+
+    /// <summary>シングルトン</summary>
+    public class Singleton<T>
+        where T : new()
+    {
+        public static T Instance;
+
+        static Singleton() {
+            Instance = new T();
+        }
+    }
+}
+
+
