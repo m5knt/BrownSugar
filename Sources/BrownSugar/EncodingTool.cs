2528a4485071d3e0e3305ffbec32242162f7717e
m5knt <m5knt@inetsbb.com>
2015-12-18 14:48:19 +0900

    エンコード周り追加

---
+++ Sources/BrownSugar/EncodingTool.cs
@@ -0,0 +1,27 @@
+﻿/**
+ * @file
+ * @brief エンコード関係
+ */
+
+using System;
+using System.Text;
+
+/*
+ *
+ */
+
+namespace ThunderEgg.BrownSugar {
+
+    /// <summary>エンコード関係</summary>
+    public class EncodingTool {
+
+        /// <summary>ボム無しUTF8エンコーダー</summary>
+        public static Encoding UTF8N;
+
+        static EncodingTool() {
+            UTF8N = new UTF8Encoding(false);
+        }
+    }
+}
+
+
